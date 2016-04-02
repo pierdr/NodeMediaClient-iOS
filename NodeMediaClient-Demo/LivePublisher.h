5a7bf9b600b0174f0d116ea672d4dad427004a33
Mingliang Chen <illuspas@gmail.com>
2016-04-02 16:25:38 +0800

    Update to 1.0.1

--- NodeMediaClient-Demo/LivePublisher.h
+++ NodeMediaClient-Demo/LivePublisher.h
@@ -1,6 +1,6 @@
 //
 //  LivePublisher.h
-//  NodeMediaClient v1.0.0
+//  NodeMediaClient v1.0.1
 //
 //  Created by Mingliang Chen on 15/8/21.
 //  Copyright (c) 2015年 NodeMedia. All rights reserved.
