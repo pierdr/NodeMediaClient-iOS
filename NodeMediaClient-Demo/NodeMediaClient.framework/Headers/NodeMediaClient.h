703a957488faef7dc659334f015ba4411d3071ed
Mingliang Chen <illuspas@gmail.com>
2017-06-15 11:41:35 +0800

    update to v2.1.3

--- NodeMediaClient-Demo/NodeMediaClient.framework/Headers/NodeMediaClient.h
+++ NodeMediaClient-Demo/NodeMediaClient.framework/Headers/NodeMediaClient.h
@@ -6,6 +6,7 @@
 // v2.1.0 - 2017/5/22
 // v2.1.1 - 2017/6/4
 // v2.1.2 - 2017/6/12
+// v2.1.3 - 2017/6/15
 //  Created by Mingliang Chen (illuspas@gmail.com) on 16/12/29.
 //  Copyright © 2017 NodeMedia. All rights reserved.
 //
