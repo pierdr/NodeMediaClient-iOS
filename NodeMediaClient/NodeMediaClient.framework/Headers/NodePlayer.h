b8278a9dbf6a6f95349c1bad3c6ae2f5a91003bb
Chen Mingliang <illuspas@msn.com>
2020-07-19 21:28:15 +0800

    update to v2.7.10

--- NodeMediaClient/NodeMediaClient.framework/Headers/NodePlayer.h
+++ NodeMediaClient/NodeMediaClient.framework/Headers/NodePlayer.h
@@ -97,10 +97,10 @@
  */
 @property (nonatomic) BOOL hwEnable;
 
-///是否开启音频 随时可以设置
+///是否开启音频
 @property (nonatomic) BOOL audioEnable;
 
-///是否开启视频 随时可以设置
+///是否开启视频
 @property (nonatomic) BOOL videoEnable;
 
 ///是否以subscribe模式播放视频
